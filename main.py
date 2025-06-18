d20393116e723e1739e66067e6cf2424e2555ecb
sudhendrakowlagi <sudhendrakowlagi5@gmail.com>
2025-06-18 13:28:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from utils.api_handler import call_api
 
 def main():
     response = call_api()
+    t = printfunction()
+    print(t)
     print("API Response:", response)
+    
 
 if __name__ == "__main__":
     main()
